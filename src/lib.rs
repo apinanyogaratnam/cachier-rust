5d0524e256f2b445d9047fb8b97c4fce718a49f9
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-31 12:46:41 -0500

    feat: created Cachier struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::time::Duration;
+use std::error::Error;
+
+pub struct Cachier {
+    url: String,
+    storage: String,
+}
+
+impl Cachier {
+    pub fn new(url: String, storage: String) -> Self {
+        return Cachier {url: url, storage: storage};
+    }
+
+    pub async fn get(&self, key: Option<&str>) {
+        match key {
+            Some(key) => {
+                let result = self.__get(key).await;
+                match result {
+                    Ok(result) => println!("{:?}", result),
+                    Err(result) => println!("{:?}", result),
+                }
+            },
+            None => println!("none"),
+        }
+    }
+
+    async fn __get(&self, key: &str) -> Result<(), Box<dyn Error>> {
+        let client = reqwest::Client::new();
+        let doge = client
+        .get(format!("{url}?cache_key={key}&driver={driver}", url=self.url, key=key, driver=self.storage))
+        .header("Accept", "text/plain")
+        .timeout(Duration::from_secs(3))
+        .send()
+        .await?
+        .text()
+        .await?;
+        println!("{:}", doge);
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +45,5 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
     }
 }
