93080e9eabb19f49e5459cc111d1c87de6cf8e1c
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-31 23:06:57 -0500

    feat: completed example code

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,10 +1,17 @@
-use cachier::cachier::Cachier;
+use reqwest::Client;
+use serde_json::json;
+use std::error::Error;
+use std::time::Duration;
 
-
-fn main() {
+#[tokio::main]
+async fn main() {
     let cachier = Cachier::new("https://example.com/api".to_string(), "memory".to_string());
-    match cachier.get(Some("key")) {
-        Ok(value) => println!("Received value: {}", value),
-        Err(err) => println!("Error: {}", err),
-    }
+
+    // Call the `set` method to store a value in the cache
+    let is_saved = cachier.set("key", json!("value"), Some(3600)).await;
+    println!("Is saved successfully: {}", is_saved);
+
+    // Call the `get` method to retrieve the value from the cache
+    let result = cachier.get(Some("key")).await;
+    println!("Result: {:?}", result);
 }
