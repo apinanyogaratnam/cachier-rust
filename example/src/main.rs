f2538ad87c237cc71a6a33df3b77d71b6a66195a
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-31 23:05:24 -0500

    refactor: progress of example

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,4 +2,9 @@ use cachier::cachier::Cachier;
 
 
 fn main() {
+    let cachier = Cachier::new("https://example.com/api".to_string(), "memory".to_string());
+    match cachier.get(Some("key")) {
+        Ok(value) => println!("Received value: {}", value),
+        Err(err) => println!("Error: {}", err),
+    }
 }
